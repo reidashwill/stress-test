1e6d3dce221ce274cda70deafec8788e67119b60
Reid Ashwill <reidashwill@gmail.com>
2020-03-31 15:27:49 -0700

    add results and display them to user after they select inputs

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
       var stressLevel = $(this).val();
       stressArray.push(stressLevel);
     })
+    console.log(stressArray.length)
 
      $("input:checkbox[name=coping]:checked").each(function(){
        var copeLevel = $(this).val();
        copeArray.push(copeLevel);
      });
+     console.log(copeArray.length)
      if (stressArray.length > copeArray.length) {
        $("#warning1").show();
      } else if (stressArray.length === copeArray.length) {
